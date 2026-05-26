0b77445c3fe96670d7193497b07683cfa3e38aeb
DubravkoSuss <108920470+DubravkoSuss@users.noreply.github.com>
2026-05-26 15:07:36 +0200

    Delete ExcelTransfer_Portable directory

--- ExcelTransfer_Portable/Launch.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Excel Transfer Launcher
-cd /d "%~dp0"
-if not exist "ExcelTransfer.exe" (
-    echo ERROR: ExcelTransfer.exe not found!
-    pause
-    exit /b 1
-)
-start "" "ExcelTransfer.exe"
